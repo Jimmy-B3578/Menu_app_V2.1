83084c8ed3a04040c6684d4e45a4818ea2bee3c0
James <jamesbrown3578@gmail.com>
2025-04-28 11:41:01 +1000

    added search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import styles from '../styles/HomeScreenStyles';
+import { View, TextInput, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Screen</Text>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Search..."
+      />
     </View>
   );
-} 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  searchBar: {
+    height: 50,
+    width: '80%',
+    borderColor: '#dfe1e5',
+    borderWidth: 1,
+    borderRadius: 25,
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontSize: 16,
+    backgroundColor: '#fff',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.20,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+}); 
